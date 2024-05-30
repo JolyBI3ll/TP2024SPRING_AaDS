5b298c787a182b2a9bf7713bd0b16cbf7dabe62a
Mamou Asman <asman2003pda45@gmail.com>
2024-05-30 14:26:37 +0300

    start project!

---
+++ ArcGraph.cpp
@@ -0,0 +1,3 @@
+//
+// Created by asman on 30.05.2024.
+//
--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include "MatrixGraph.h"
+#include "ListGraph.h"
+#include "SetGraph.h"
+#include "ArcGraph.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
